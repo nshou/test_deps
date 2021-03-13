884dae7812a8ff7718724aff97eee352fc915640
nshou <nshou@coronocoya.net>
2021-03-13 00:26:01 -0800

    A macro just inserting in and out print

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use proc_macro::TokenStream;
+use quote;
+use syn::{self, Block, ItemFn};
+
+#[proc_macro_attribute]
+pub fn trace(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut ast = syn::parse_macro_input!(item as ItemFn);
+    //let ident = &ast.ident;
+    let ident = "fake_func_name";
+
+    let body = ast.block.as_ref();
+    let body: Block = syn::parse_quote! {{
+        println!("in: {}", stringify!(#ident));
+
+        let body = || #body;
+        let ret = body();
+
+        println!("out: {}", stringify!(#ident));
+        ret
+    }};
+
+    *ast.block = body;
+    let gen = quote::quote! {
+        #ast
+    };
+    gen.into()
 }
